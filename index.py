ca8b67226dfaa806113844af0400e6302ca4c1ee
Karthik Rishinarada <karthikrk11135@gmail.com>
2025-03-09 22:23:32 +0530

    update with lines_all and levels_all

--- index.py
+++ index.py
@@ -3,24 +3,29 @@
 
 from flask import render_template, Flask
 import pandas as pd
+from carsus.io.nist import NISTWeightsComp, NISTIonizationEnergies
+from carsus.io.kurucz import GFALLReader
+from carsus.io.zeta import KnoxLongZeta
+from carsus.io.output import TARDISAtomData
+
+zeta_data = KnoxLongZeta()
+
+ionization_energies = NISTIonizationEnergies('H-Zn')
+atomic_weights = NISTWeightsComp()
+
+gfall_reader = GFALLReader('H-Zn',
+                           '/tmp/gfall.dat')
+
+atom_data = TARDISAtomData(atomic_weights,
+                           ionization_energies,
+                           gfall_reader,
+                           zeta_data,)
+
 
 app = Flask(__name__)
 
-path_to_atomic_file = './kurucz_cd23_chianti_Si.h5' # path of the atomic file goes here (can be found in tardis regression data)
-
-# Change the values to empty df when using atomic file
-lines_data = pd.read_csv("./lines_df.csv")
-levels_data = pd.read_csv("./levels_df.csv")
-
-# Uncomment this once, you've set your path
-# with pd.HDFStore(path_to_atomic_file, mode='a') as store:
-#     line_df = store.select('/lines_data')
-#     line_df = line_df.reset_index()
-#     lines_data = line_df.iloc[:50]
-
-#     level_df = store.select('/levels_data')
-#     level_df = level_df.reset_index()
-#     levels_data = level_df.iloc[:50]
+lines_data = atom_data.lines_all.reset_index()[:50]
+levels_data = atom_data.levels_all.reset_index()[:50]
 
 
 lines_arr = []
@@ -28,37 +33,41 @@ levels_arr = []
 
 for index, row in lines_data.iterrows():
     obj = {
-        'atomic_number': row['atomic_number'],
-        'ion_number': row['ion_number'],
-        'level_number_lower': row['level_number_lower'],
-        'level_number_upper': row['level_number_upper'],
         'line_id': row['line_id'],
+        'lower_level_id': row['lower_level_id'],
+        'upper_level_id': row['upper_level_id'],
         'wavelength': row['wavelength'],
-        'f_ul': row['f_ul'],
-        'f_lu': row['f_lu'],
-        'nu': row['nu'],
-        'B_lu': row['B_lu'],
-        'B_ul': row['B_ul'], 
-        'A_ul': row['A_ul'], 
+        'gf': row['gf'],
+        'loggf': row['loggf'],
+        'ds_id': row['ds_id'],
+        # 'level_number_upper': row['level_number_upper'],
+        # 'wavelength': row['wavelength'],
+        # 'f_ul': row['f_ul'],
+        # 'f_lu': row['f_lu'],
+        # 'nu': row['nu'],
+        # 'B_lu': row['B_lu'],
+        # 'B_ul': row['B_ul'], 
+        # 'A_ul': row['A_ul'],   
     } 
     lines_arr.append(obj)
 
 for index, row in levels_data.iterrows():
     obj = {
+        'index': row['index'],
+        'level_id': row['level_id'],
         'atomic_number': row['atomic_number'],
         'ion_number': row['ion_number'],
-        'level_number': row['level_number'],
         'g': row['g'],
         'energy': row['energy'],
-        'metastable': row['metastable'],
+        # 'metastable': row['metastable'],
+        'ds_id': row['ds_id'],
     } 
     levels_arr.append(obj)
 
 
 @app.route("/")
 def index():
-    return render_template("atomic_data.html", lines_arr = lines_arr, levels_arr=levels_arr)
-
+    return render_template("atomic_data.html", levels_arr=levels_arr, lines_arr=lines_arr)
 
 if __name__ == "__main__":
     app.run(debug=True)
