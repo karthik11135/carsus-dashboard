7b2a0df50a6e0e6062079074c89c2ced88784de3
Karthik Rishinarada <karthikrk11135@gmail.com>
2025-03-14 16:26:39 +0530

    si based filter

--- index.py
+++ index.py
@@ -17,33 +17,40 @@ atom_data = TARDISAtomData(atomic_weights,
                            gfall_reader,
                            zeta_data,)
 
-lines_data = atom_data.lines_all.reset_index()[:50]
-levels_data = atom_data.levels_all.reset_index()[:50]
+df1 = atom_data.lines_prepared.reset_index()
+df2 = atom_data.levels_prepared.reset_index()
+
+lines_data = df1[df1["atomic_number"] == 14].iloc[:50]
+levels_data = df2[df2["atomic_number"] == 14].iloc[:50]
 
 lines_arr = []
 levels_arr = []
 
 for index, row in lines_data.iterrows():
     obj = {
+        'atomic_number': row['atomic_number'],
+        'ion_number': row['ion_number'],
+        'level_number_lower': row['level_number_lower'],
+        'level_number_upper': row['level_number_upper'],
         'line_id': row['line_id'],
-        'lower_level_id': row['lower_level_id'],
-        'upper_level_id': row['upper_level_id'],
         'wavelength': row['wavelength'],
-        'gf': row['gf'],
-        'loggf': row['loggf'],
-        'ds_id': row['ds_id'],
+        'f_ul': row['f_ul'],
+        'f_lu': row['f_lu'],
+        'nu': row['nu'],
+        'B_ul': row['B_ul'],
+        'B_lu': row['B_lu'],
+        'A_ul': row['A_ul'],
     } 
     lines_arr.append(obj)
 
 for index, row in levels_data.iterrows():
     obj = {
-        'index': row['index'],
-        'level_id': row['level_id'],
         'atomic_number': row['atomic_number'],
         'ion_number': row['ion_number'],
-        'g': row['g'],
+        'level_number': row['level_number'],
         'energy': row['energy'],
-        'ds_id': row['ds_id'],
+        'g': row['g'],
+        'metastable': row['metastable'],
     } 
     levels_arr.append(obj)
 
